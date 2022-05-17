a1290cd49c8b93747d7770f4d3f7b9b36e70c03b
Raphael Haasper <raphaelhaasper@gmail.com>
2022-05-17 13:57:54 +0200

    changed how .env are loaded. From .env file to .env inside next.config

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,7 +1,7 @@
 import { createContext , useEffect, useState,} from "react"
 import {setCookie, parseCookies} from 'nookies';
 
-import {api} from './services/api';
+import {api} from '../services/api';
 
 
 import Router from 'next/router';
--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['github.com']
+  },
+  env: {
+    MONGO: "mongodb+srv://RaphaelCH:577uLumAb9wynY7@cluster0.zjqfk.mongodb.net/BeVisible?retryWrites=true&w=majority"
   }
 }
 
