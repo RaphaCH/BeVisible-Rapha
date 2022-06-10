d6e6024eb4e2661555d7771fffc00876359c728d
Raphael Haasper <raphaelhaasper@gmail.com>
2022-06-10 13:11:00 +0200

    UPDATE: added HEAD tag to register Personnel page

--- pages/registerCompany.js
+++ pages/registerCompany.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import { useState } from "react";
 import {useRouter} from 'next/router';
 import Image from 'next/image';
+import Head from 'next/head';
 
 import AppInputWithIcon from "../components/AppInputWithIcon";
 import Button from "../components/Button";
@@ -65,73 +66,79 @@ export default function RegisterCompany() {
   }
 
   return (
-    <div className="h-screen flex">
-      <div className="hidden lg:flex w-full lg:w-1/2 
-        bg-beCodeLight bg-auto bg-no-repeat bg-right
-          justify-around items-center">
-        <div className="w-1/2 mx-auto px-20 flex-col items-center space-y-6">
-          <h1 className="text-white font-bold text-4xl font-sans">BeVisible</h1>
-          <h2 className="text-white mt-1">Flood the market with your talent.</h2>
-          {/* <div className="flex justify-center lg:justify-start mt-6">
-            <a href="#" className="hover:bg-indigo-700 hover:text-white hover:-translate-y-1 transition-all duration-500 bg-white text-indigo-800 mt-4 px-4 py-2 rounded-2xl font-bold mb-2">Get Started</a>
-          </div> */}
+    <div>
+      <Head>
+        <title>Manage Personnel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
+      <div className="h-screen flex">
+        <div className="hidden lg:flex w-full lg:w-1/2 
+          bg-beCodeLight bg-auto bg-no-repeat bg-right
+            justify-around items-center">
+          <div className="w-1/2 mx-auto px-20 flex-col items-center space-y-6">
+            <h1 className="text-white font-bold text-4xl font-sans">BeVisible</h1>
+            <h2 className="text-white mt-1">Flood the market with your talent.</h2>
+            {/* <div className="flex justify-center lg:justify-start mt-6">
+              <a href="#" className="hover:bg-indigo-700 hover:text-white hover:-translate-y-1 transition-all duration-500 bg-white text-indigo-800 mt-4 px-4 py-2 rounded-2xl font-bold mb-2">Get Started</a>
+            </div> */}
+          </div>
+          <div className="w-1/2">
+            <Image 
+            width={470} 
+            height={470} 
+            alt='logo of the company' 
+            src="/images/logo-becode.png"
+            layout="responsive"
+            />
+          </div>
+          
         </div>
-        <div className="w-1/2">
-          <Image 
-          width={470} 
-          height={470} 
-          alt='logo of the company' 
-          src="/images/logo-becode.png"
-          layout="responsive"
-           />
-        </div>
-         
-      </div>
-      <div className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8">
-        <div className="w-full px-8 md:px-32 lg:px-24">
-          <form onSubmit={handleSubmit}  className="bg-white rounded-md shadow-2xl p-5">
-            <h1 className="text-gray-800 font-bold text-2xl mb-1">Welcome Coach!</h1>
-            <p className="text-sm font-normal text-gray-600 mb-8">Here you can add new coaches or companies.</p>
-            <p className="font-bold self-center text-red-500">{message}</p>
-            <AppInputWithIcon 
-              required 
-              icon={faAt} 
-              type='email' 
-              name='email' 
-              value={form.email} 
-              onChange={handleChange} 
-              placeholder='Email Address' />
-            <p>{form.email}</p>
-            <AppInputWithIcon 
-              required 
-              // pattern='[0-9]' 
-              minLength={6} 
-              icon={faLock} 
-              type='password' 
-              name='password' 
-              value={form.password} 
-              title='Password should be digits (0-9) with minimum 6 characters'
-              onChange={handleChange} 
-              placeholder='Set a password' />
-            <p>{form.password}</p>
-            <AppInputWithIcon 
-              required 
-              // pattern='[0-9]' 
-              minLength={6} 
-              icon={faLock} 
-              type='password' 
-              name='password2' 
-              value={form.password2} 
-              title='Password should be digits (0-9) with minimum 6 characters'
-              onChange={handleChange} 
-              placeholder='Repeat it' />
-            <p>{form.password2}</p>
-            <div className="flex flex-row justify-evenly">
-              <CheckBox name='Coach' value={coach} onChange={handleStudent} />
-              <CheckBox name='Company' value={company} onChange={handleCompany} />
-            </div>
-            <Button color="primary" name='Register' type="submit" />
-          </form>
+        <div className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8">
+          <div className="w-full px-8 md:px-32 lg:px-24">
+            <form onSubmit={handleSubmit}  className="bg-white rounded-md shadow-2xl p-5">
+              <h1 className="text-gray-800 font-bold text-2xl mb-1">Welcome Coach!</h1>
+              <p className="text-sm font-normal text-gray-600 mb-8">Here you can add new coaches or companies.</p>
+              <p className="font-bold self-center text-red-500">{message}</p>
+              <AppInputWithIcon 
+                required 
+                icon={faAt} 
+                type='email' 
+                name='email' 
+                value={form.email} 
+                onChange={handleChange} 
+                placeholder='Email Address' />
+              <p>{form.email}</p>
+              <AppInputWithIcon 
+                required 
+                // pattern='[0-9]' 
+                minLength={6} 
+                icon={faLock} 
+                type='password' 
+                name='password' 
+                value={form.password} 
+                title='Password should be digits (0-9) with minimum 6 characters'
+                onChange={handleChange} 
+                placeholder='Set a password' />
+              <p>{form.password}</p>
+              <AppInputWithIcon 
+                required 
+                // pattern='[0-9]' 
+                minLength={6} 
+                icon={faLock} 
+                type='password' 
+                name='password2' 
+                value={form.password2} 
+                title='Password should be digits (0-9) with minimum 6 characters'
+                onChange={handleChange} 
+                placeholder='Repeat it' />
+              <p>{form.password2}</p>
+              <div className="flex flex-row justify-evenly">
+                <CheckBox name='Coach' value={coach} onChange={handleStudent} />
+                <CheckBox name='Company' value={company} onChange={handleCompany} />
+              </div>
+              <Button color="primary" name='Register' type="submit" />
+            </form>
+          </div>
         </div>
       </div>
     </div>
